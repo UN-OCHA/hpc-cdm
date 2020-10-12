7facfc21aeff21787af48a0fa2df0510e2b43953
Sam Lanning <sam@samlanning.com>
2020-10-12 14:43:38 +0100

    Replace caret icon with one from design

--- libs/hpc-ui/src/lib/icons/caret.tsx
+++ libs/hpc-ui/src/lib/icons/caret.tsx
@@ -17,11 +17,11 @@ export default (props: Props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 2.646 2.646"
-      width={10}
+      viewBox="0 0 1.764 1.058"
+      width={6.6}
       {...props}
     >
-      <path d="M2.237.53l-.914 1.585L.408.531z" fill="currentColor" />
+      <path d="M1.764.15A.151.151 0 001.72.046a.143.143 0 00-.209 0L.884.69.253.045a.143.143 0 00-.209 0 .153.153 0 000 .215l.734.753c.058.06.15.06.208 0L1.72.26a.167.167 0 00.044-.11z" fill="currentColor" />
     </svg>
   );
 };
