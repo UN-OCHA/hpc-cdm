4ed385aedde5740432277940c1f676a2e90974ff
Petar Petković <pl_217@live.com>
2021-01-17 23:44:41 +0100

    Remove unused import in libs/hpc-live/src/lib/live.ts

--- libs/hpc-live/src/lib/live.ts
+++ libs/hpc-live/src/lib/live.ts
@@ -1,7 +1,6 @@
 import { UserManager, User, OidcMetadata } from 'oidc-client';
 
 import { config, Session } from '@unocha/hpc-core';
-import { Model } from '@unocha/hpc-data';
 
 import { LiveModel } from './model';
 
