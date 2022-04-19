7c08b193ec8a74dc4615065da57ef8501f48e6c3
Pl217 <pl_217@live.com>
2022-04-19 13:05:30 +0200

    Remove Arabic import
    Arabic was removed in 8269422c038d2b4c16638eee0c0829aba37fdf03

--- apps/hpc-cdm/src/i18n/index.ts
+++ apps/hpc-cdm/src/i18n/index.ts
@@ -1,17 +1,14 @@
 import { i18n } from '@unocha/hpc-core';
 import { Translations } from '@unocha/hpc-ui';
 
-import ar from './langs/ar';
 import en from './langs/en';
 import fr from './langs/fr';
 
-import 'intl-list-format/locale-data/ar';
 import 'intl-list-format/locale-data/en';
 import 'intl-list-format/locale-data/fr';
 
 const LANGUAGES = {
   en,
-  // ar: merge({}, en, ar),
   fr,
 };
 
