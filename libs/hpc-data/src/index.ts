62629d79302aac6a27a460f9be8746436c103a9a
Clara Jordan <clara.jordan@hpc.tools>
2022-05-26 12:30:16 +0100

    Get flows data

--- libs/hpc-data/src/index.ts
+++ libs/hpc-data/src/index.ts
@@ -1,14 +1,16 @@
 import * as access from './lib/access';
+import * as errors from './lib/errors';
+import * as flows from './lib/flows';
 import * as forms from './lib/forms';
 import * as operations from './lib/operations';
 import * as reportingWindows from './lib/reporting-windows';
-import * as errors from './lib/errors';
 import * as util from './lib/util';
 
 export interface Model {
   access: access.Model;
+  flows: flows.Model;
   operations: operations.Model;
   reportingWindows: reportingWindows.Model;
 }
 
-export { access, forms, operations, reportingWindows, errors, util };
+export { access, forms, flows, operations, reportingWindows, errors, util };
