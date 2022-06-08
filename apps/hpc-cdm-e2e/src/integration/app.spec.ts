d49762f2c1b2ba549965063610b7138d24f3a6dd
Clara Jordan <clara.jordan@hpc.tools>
2022-06-08 10:00:02 +0100

    Make cypress tests pass for CDM

--- apps/hpc-cdm-e2e/src/integration/app.spec.ts
+++ apps/hpc-cdm-e2e/src/integration/app.spec.ts
@@ -1,13 +1,12 @@
-import { getGreeting } from '../support/app.po';
-
 describe('hpc-cdm', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('redirects to /operations when logged in', () => {
+    cy.login('Admin User');
+    cy.url().should('include', '/operations');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to hpc-cdm!');
+  it("doesn't redirect if not logged in", () => {
+    cy.url().should('not.include', '/operations');
   });
 });
