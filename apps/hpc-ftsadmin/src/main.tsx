c18d57a81851e293304a1f1e76d49fa1589771ee
Clara Jordan <clara.jordan@hpc.tools>
2022-06-15 08:38:57 +0100

    Add query strings to flows table

--- apps/hpc-ftsadmin/src/main.tsx
+++ apps/hpc-ftsadmin/src/main.tsx
@@ -1,11 +1,15 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter5Adapter } from 'use-query-params/adapters/react-router-5';
 
 import App from './app/app';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <QueryParamProvider adapter={ReactRouter5Adapter}>
+      <App />
+    </QueryParamProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
