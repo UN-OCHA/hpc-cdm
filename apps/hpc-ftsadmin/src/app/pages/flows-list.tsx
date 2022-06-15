9ce619b5dcba718237bd2fd312528fc365551d68
Clara Jordan <clara.jordan@hpc.tools>
2022-06-15 09:57:37 +0100

    Make FlowsTable headers configurable as a prop

--- apps/hpc-ftsadmin/src/app/pages/flows-list.tsx
+++ apps/hpc-ftsadmin/src/app/pages/flows-list.tsx
@@ -1,6 +1,6 @@
 import { C, CLASSES, combineClasses } from '@unocha/hpc-ui';
 import { t } from '../../i18n';
-import FlowsTable from '../components/flows-table';
+import FlowsTable, { FlowsTableProps } from '../components/flows-table';
 import PageMeta from '../components/page-meta';
 import { AppContext } from '../context';
 
@@ -9,6 +9,60 @@ interface Props {
 }
 
 export default (props: Props) => {
+  const flowsTableProps: FlowsTableProps = {
+    headers: [
+      {
+        id: 'flow.id',
+        sortable: true,
+        label: 'id',
+      },
+      {
+        id: 'flow.updatedAt',
+        sortable: true,
+        label: 'updatedCreated',
+      },
+      {
+        id: 'externalReference.systemID',
+        sortable: true,
+        label: 'dataProvider',
+      },
+      {
+        id: 'flow.amountUSD',
+        sortable: true,
+        label: 'amountUSD',
+      },
+      {
+        id: 'source.organization.name',
+        sortable: true,
+        label: 'sourceOrganization',
+      },
+      {
+        id: 'destination.organization.name',
+        sortable: true,
+        label: 'destinationOrganization',
+      },
+      {
+        id: 'destination.planVersion.name',
+        sortable: true,
+        label: 'destinationPlan',
+      },
+      {
+        id: 'destination.location.name',
+        sortable: true,
+        label: 'destinationCountry',
+      },
+      {
+        id: 'destination.usageYear.year',
+        sortable: true,
+        label: 'destinationYear',
+      },
+      {
+        id: 'details',
+        label: 'details',
+      },
+    ],
+  };
+
   return (
     <AppContext.Consumer>
       {({ lang }) => (
@@ -17,7 +71,7 @@ export default (props: Props) => {
         >
           <PageMeta title={[t.t(lang, (s) => s.routes.flows.title)]} />
           <C.PageTitle>{t.t(lang, (s) => s.routes.flows.title)}</C.PageTitle>
-          <FlowsTable />
+          <FlowsTable headers={flowsTableProps.headers} />
         </div>
       )}
     </AppContext.Consumer>
