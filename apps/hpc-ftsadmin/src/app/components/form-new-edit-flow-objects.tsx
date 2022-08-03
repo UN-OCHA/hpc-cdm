2fc6d0d6c7793faf3d0cdb3fb4a2d4e55e4d1267
Clara Jordan <clara.jordan@hpc.tools>
2022-08-03 10:17:49 +0100

    Make fields deletable

--- apps/hpc-ftsadmin/src/app/components/form-new-edit-flow-objects.tsx
+++ apps/hpc-ftsadmin/src/app/components/form-new-edit-flow-objects.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Box, Button, IconButton, Typography } from '@mui/material';
 import {
   emergencies,
   globalClusters,
@@ -10,8 +10,10 @@ import {
   usageYears,
 } from '@unocha/hpc-data';
 import { styled } from '@unocha/hpc-ui';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { MdAdd, MdRemoveCircleOutline } from 'react-icons/md';
+import { t } from '../../i18n';
 import { AppContext, getEnv } from '../context';
 import FormNewEditFlowObjectControl, {
   FormNewEditFlowObjectControlProps,
@@ -28,7 +30,13 @@ interface Props {
   label: string;
 }
 
-type Field<T> = Omit<FormNewEditFlowObjectControlProps<T>, 'refDirection' | 'objectType'>;
+interface Field<T>
+  extends Omit<
+    FormNewEditFlowObjectControlProps<T>,
+    'refDirection' | 'objectType'
+  > {
+  isDeletable?: boolean;
+}
 
 interface Fields {
   organization: Field<organizations.Organization>;
@@ -45,7 +53,7 @@ interface Fields {
 export default function FormNewEditFlowObjects(props: Props) {
   const { refDirection, label } = props;
   const { model } = getEnv();
-  const { watch } = useFormContext();
+  const { watch, getValues } = useFormContext();
 
   const watchPlan = watch(`${refDirection}.plan`);
   const watchDestOrgs = watch('destination.organization');
@@ -69,6 +77,7 @@ export default function FormNewEditFlowObjects(props: Props) {
             s.components.forms.newEditFlow.fields.anonymizedOrganizations,
           getOptions: model.organizations.getOrganizationsAutocomplete,
           searchOnType: true,
+          isDeletable: true,
         },
       }),
       usageYear: {
@@ -85,31 +94,55 @@ export default function FormNewEditFlowObjects(props: Props) {
         label: (s) => s.components.forms.newEditFlow.fields.plans,
         getOptions: model.plans.getPlansAutocomplete,
         searchOnType: true,
+        isDeletable: refDirection === 'source',
       },
       project: {
         label: (s) => s.components.forms.newEditFlow.fields.projects,
         getOptions: model.projects.getProjectsAutocomplete,
         searchOnType: true,
+        isDeletable: true,
       },
       emergency: {
         label: (s) => s.components.forms.newEditFlow.fields.emergencies,
         getOptions: model.emergencies.getEmergenciesAutocomplete,
         searchOnType: true,
+        isDeletable: true,
       },
       globalCluster: {
         label: (s) => s.components.forms.newEditFlow.fields.globalClusters,
         getOptions: model.globalClusters.getGlobalClusters,
+        isDeletable: refDirection === 'source',
       },
       ...(showGoverningEntities && {
         governingEntity: {
           label: (s) => s.components.forms.newEditFlow.fields.governingEntities,
           getOptions: model.governingEntities.getGoverningEntitiesAutocomplete,
           searchOnType: true,
+          isDeletable: true,
         },
       }),
     }),
-    [model, showGoverningEntities, showAnonymizedOrgs]
+    [model, refDirection, showGoverningEntities, showAnonymizedOrgs]
   );
+
+  const [visibleFields, setVisibleFields] = useState(
+    new Set(
+      (Object.keys(fields) as (keyof Fields)[]).filter(
+        // TypeScript types the return value of the Object.keys() method as string[].
+        (key) =>
+          !fields[key]?.isDeletable ||
+          getValues(`${refDirection}.${key}`).length
+      )
+    )
+  );
+  const setVisible = (key: keyof Fields) => {
+    setVisibleFields(new Set(visibleFields).add(key));
+  };
+  const setHidden = (key: keyof Fields) => {
+    const visible = new Set(visibleFields);
+    visible.delete(key);
+    setVisibleFields(visible);
+  };
 
   return (
     <AppContext.Consumer>
@@ -118,14 +151,46 @@ export default function FormNewEditFlowObjects(props: Props) {
           <Typography variant="h3" component="legend" sx={{ marginBottom: 2 }}>
             {label}
           </Typography>
-          {Object.entries(fields).map(([key, { ...props }]) => (
-            <FormNewEditFlowObjectControl
-              key={`${refDirection}.${key}`}
-              refDirection={refDirection}
-              objectType={key}
-              {...props}
-            />
-          ))}
+          {Object.entries(fields).map(([key, { isDeletable, ...rest }]) =>
+            visibleFields.has(key as keyof Fields) ? (
+              <Box
+                sx={{ display: 'flex', alignItems: 'flex-start' }}
+                key={`${refDirection}.${key}`}
+              >
+                <FormNewEditFlowObjectControl
+                  refDirection={refDirection}
+                  objectType={key}
+                  {...rest}
+                />
+                {isDeletable && (
+                  <IconButton
+                    aria-label={t.get(lang, (s) => s.common.remove)}
+                    sx={{ marginLeft: 1, marginTop: 3 }}
+                    onClick={() => setHidden(key as keyof Fields)}
+                  >
+                    <MdRemoveCircleOutline />
+                  </IconButton>
+                )}
+              </Box>
+            ) : null
+          )}
+          <Box sx={{ marginY: 2 }}>
+            {Object.entries(fields).map(([key, { label }]) =>
+              !visibleFields.has(key as keyof Fields) ? (
+                <Button
+                  key={`autocompleteButton.${refDirection}.${key}`}
+                  startIcon={
+                    <MdAdd aria-label={t.get(lang, (s) => s.common.add)} />
+                  }
+                  variant="outlined"
+                  sx={{ marginRight: 1, marginBottom: 1 }}
+                  onClick={() => setVisible(key as keyof Fields)}
+                >
+                  {t.get(lang, label)}
+                </Button>
+              ) : null
+            )}
+          </Box>
         </Fieldset>
       )}
     </AppContext.Consumer>
