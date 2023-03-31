6fe2e5008a51bed4e73cca0e129e0d1b382677db
Antonio <anpesalado@gmail.com>
2023-03-31 15:30:20 +0200

    HPC-9035: Don't bundle entire package.json in CDM
    Just importing a version from package.json with:
    `import { version } from './package.json';`
    actually causes entire package.json to be shipped.
    
    To avoid leaking of package.json content, but also
    to reduce the bundle size, the global variable is
    inserted via Webpack, which avoids loading entire
    package.json in our application code.

--- apps/hpc-cdm/webpack.js
+++ apps/hpc-cdm/webpack.js
@@ -1,9 +1,24 @@
 /* eslint-disable */
 const path = require('path');
 const upstreamConfig = require('@nrwl/react/plugins/webpack');
+const webpack = require('webpack');
 
 module.exports = (config) => {
   config = upstreamConfig(config);
+
+  /*
+   * To avoid loading version from package.json in application code,
+   * which causes unintentional bundling of entire package.json, we
+   * are injecting a global variable to expose just the app version.
+   *
+   * See HPC-9035
+   */
+  config.plugins ??= [];
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      APP_VERSION: JSON.stringify(require('../../package.json').version),
+    })
+  );
 
   // Override enketo modules
   config.resolve.alias['enketo/dialog'] = path.resolve(
