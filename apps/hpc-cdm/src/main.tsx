7fe4e44a193b521f2ac364544dda989af01b820d
Onitoxan <anpesalado@gmail.com>
2023-05-01 12:02:45 +0200

    HPC-8480: Upgrade to React 18
    Changes to how React is attached to root DOM element
    are done according to this migration guide
    https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis

--- apps/hpc-cdm/src/main.tsx
+++ apps/hpc-cdm/src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@unocha/hpc-ui';
 
 import App from './app/app';
 import './assets/styles/enketo.css';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Failed to find the root element');
+}
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <BrowserRouter>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
