38aa5f19aa4b8b6b5c5ca51672bd0748a96bb8ce
Onitoxan <anpesalado@gmail.com>
2024-01-10 16:37:15 +0100

    Add paths

--- apps/hpc-ftsadmin/src/app/paths.ts
+++ apps/hpc-ftsadmin/src/app/paths.ts
@@ -1,5 +1,13 @@
 const HOME = '/';
+const ROOT = '/*';
 const FLOWS = '/flows';
+const FLOWS_REST = '/flowsREST'; // TO DO: Remove once shown to client
+const PENDING_FLOWS = '/pending-flows';
+const ORGANIZATIONS = '/organizations';
+const ORGANIZATION = `${ORGANIZATIONS}/:id`;
+const ADD_ORGANIZATION = `${ORGANIZATIONS}/add`;
+const FLOW = `${FLOWS}/:id`;
+const KEYWORDS = '/keywords';
 
 const replacePlaceholders = (
   path: string,
@@ -14,3 +22,20 @@ const replacePlaceholders = (
 export const home = () => replacePlaceholders(HOME, {});
 
 export const flows = () => replacePlaceholders(FLOWS, {});
+
+export const flowsREST = () => replacePlaceholders(FLOWS_REST, {}); // TO DO: Remove once shown to client
+
+export const pendingFlows = () => replacePlaceholders(PENDING_FLOWS, {});
+
+export const organizations = () => replacePlaceholders(ORGANIZATIONS, {});
+
+export const organization = (id: number) =>
+  replacePlaceholders(ORGANIZATION, { id });
+
+export const organizationRoot = () => ORGANIZATION + ROOT;
+
+export const addOrganization = () => replacePlaceholders(ADD_ORGANIZATION, {});
+
+export const flow = (id: number) => replacePlaceholders(FLOW, { id });
+
+export const keywords = () => replacePlaceholders(KEYWORDS, {});
