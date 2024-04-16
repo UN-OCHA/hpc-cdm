62387c1a9e739dfd5f88594ef3d0f76037fa41a8
Onitoxan <anpesalado@gmail.com>
2024-04-16 14:28:45 +0200

    Fix form-validation

--- apps/hpc-ftsadmin/src/app/utils/form-validation.ts
+++ apps/hpc-ftsadmin/src/app/utils/form-validation.ts
@@ -3,29 +3,36 @@ import { isRight } from 'fp-ts/lib/Either';
 import { FormikErrors } from 'formik';
 import { isKey } from './parse-filters';
 
+export const parseFieldError = (validationError: string, error: string) => {
+  if (validationError === '{validationError}') {
+    return error;
+  }
+};
+const isStringUndefined = (value: unknown): value is string | undefined =>
+  typeof value === 'string' || typeof value === 'undefined';
 /**
  * Validate form fields when using io-ts as a validator. validationSchema key names must be the same as the ones supplied to values
  */
-const validateForm = <T extends object>(
+const validateForm = <T extends object, K extends io.Any>(
   values: T,
-  validationSchema: io.Any
+  validationSchema: K
 ) => {
   const result = validationSchema.decode(values);
   if (isRight(result)) {
     return {};
   } else {
     const errors: FormikErrors<T> = {};
-    result.left.map((value) =>
-      value.context.map((context) => {
+    for (const value of result.left) {
+      for (const context of value.context) {
         if (isKey(values, context.key)) {
           const key = context.key;
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          //@ts-ignore
-          errors[key] = '{validationError}'; //  Placeholder to change for i18n text
+          //  Did not find a better solution, but I think it is fine to let it like that for the moment
+          if (isStringUndefined(errors[key])) {
+            (errors[key] as string | undefined) = '{validationError}'; //  Placeholder to change for i18n text
+          }
         }
-        return null;
-      })
-    );
+      }
+    }
     return errors;
   }
 };
