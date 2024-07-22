368a6e4d307eb991bfd9a93296529c054de3230d
Onitoxan <anpesalado@gmail.com>
2024-07-22 13:19:20 +0200

    Remove unused functions

--- apps/hpc-ftsadmin/src/app/utils/map-functions.ts
+++ apps/hpc-ftsadmin/src/app/utils/map-functions.ts
@@ -1,28 +1,9 @@
-import { organizations, FormObjectValue } from '@unocha/hpc-data';
+import { organizations } from '@unocha/hpc-data';
 import { LanguageKey } from '../../i18n';
 import dayjs from 'dayjs';
 
 export const valueToInteger = (value: string | number) => {
   return typeof value === 'number' ? value : parseInt(value);
-};
-
-export const formValueToID = (items: Array<FormObjectValue>): number[] => {
-  return items.map((item) => valueToInteger(item.value));
-};
-
-export const formValueToLabel = (items: Array<FormObjectValue>): string[] => {
-  return items.map((item) => item.displayLabel);
-};
-
-export const camelCaseToTitle = (word: string): string => {
-  const addSpaces = word.replace(/([A-Z]+)/g, ' $1');
-  return addSpaces.charAt(0).toUpperCase() + addSpaces.slice(1);
-};
-
-export const stringToPendingFlowSelect = (
-  items: string[]
-): { id: number; versionID: number }[] => {
-  return items.map((x) => JSON.parse(x) as { id: number; versionID: number });
 };
 
 export const parseUpdatedCreatedBy = (
