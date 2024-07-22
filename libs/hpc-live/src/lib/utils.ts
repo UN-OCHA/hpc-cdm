58322816e06cdffa9980ba66dba48d29eaa5e001
Onitoxan <anpesalado@gmail.com>
2024-07-22 12:19:18 +0200

    Refactor searchFlowParams() to be shorter and more comprenhensible

--- libs/hpc-live/src/lib/utils.ts
+++ libs/hpc-live/src/lib/utils.ts
@@ -1,112 +1,38 @@
 import { flows } from '@unocha/hpc-data';
 
-/**
- * Transform any object or Array to a string
+/** JSON.stringify() but we remove '"' from property keys.
+ *  Ex: "{\"key1\":12}" becomes: "{key1:12}"
  */
-function parseFilterToGraphQL(obj: Record<string, any>): string {
-  const keyValuePairs: string[] = [];
-
-  if (Array.isArray(obj)) {
-    // If the input is an array, apply the function to each element and return the result in square brackets
-    const arrayValues = obj.map((element: any) =>
-      parseFilterToGraphQL(element)
-    );
-    return `[${arrayValues.join(', ')}]`;
-  }
-  for (const key in obj) {
-    if (
-      Object.prototype.hasOwnProperty.call(obj, key) &&
-      obj[key] !== undefined
-    ) {
-      let valueString: string;
-
-      if (Array.isArray(obj[key])) {
-        // If the value is an array, stringify the entire array
-        valueString = `${key}: [${obj[key]
-          .map((element: any) => JSON.stringify(element))
-          .join(', ')}]`;
-      } else if (typeof obj[key] === 'object') {
-        // If the value is an object, call the function recursively
-        valueString = `${key}: ${parseFilterToGraphQL(obj[key])}`;
-      } else {
-        // Otherwise, use the value as is
-        valueString = `${key}: ${
-          typeof obj[key] === 'string' ? `"${obj[key]}"` : obj[key].toString()
-        }`;
-      }
-
-      keyValuePairs.push(valueString);
-    }
-  }
-
-  return `{${keyValuePairs.join(', ')}}`;
-}
+const stringify = (value: unknown): string =>
+  JSON.stringify(value).replace(/"([^"]+)":/g, '$1:');
 
 export const searchFlowsParams = (params: flows.SearchFlowsParams): string => {
   let queryParams = '';
-  let key: keyof typeof params;
-  for (key in params) {
-    const unmutableKey = key;
-    switch (unmutableKey) {
-      case 'carryover':
-      case 'commitment':
-      case 'paid':
-      case 'parked':
-      case 'standard':
-      case 'pass_through':
-      case 'pending':
-      case 'pledge':
-      case 'includeChildrenOfParkedFlows':
-        if (params[unmutableKey])
-          queryParams = queryParams.concat(unmutableKey, ': true ');
-        break;
-      case 'limit':
-        if (params[unmutableKey])
-          queryParams = queryParams.concat(
-            unmutableKey,
-            `:${params[unmutableKey]} `
-          );
-        break;
-      case 'sortField':
-      case 'nextPageCursor':
-      case 'prevPageCursor':
-      case 'sortOrder':
-        if (params[unmutableKey])
-          queryParams = queryParams.concat(
-            unmutableKey,
-            `:"${params[unmutableKey]}" `
-          );
-        break;
+  let typedKey: keyof typeof params;
+  for (typedKey in params) {
+    const key = typedKey;
+    switch (key) {
       case 'flowCategoryFilters':
       case 'flowObjectFilters': {
-        const filter = params[unmutableKey];
+        const filter = params[key];
         if (filter && filter.length > 0)
-          queryParams = queryParams.concat(
-            unmutableKey,
-            `:${parseFilterToGraphQL(filter)} `
-          );
+          queryParams += `${key}: ${stringify(filter)} `;
         break;
       }
+      case 'nestedFlowFilters':
       case 'flowFilters': {
-        const filter = params[unmutableKey];
+        const filter = params[key];
         if (filter && JSON.stringify(filter) !== '{}')
-          queryParams = queryParams.concat(
-            unmutableKey,
-            `:${parseFilterToGraphQL(filter)} `
-          );
+          queryParams += `${key}: ${stringify(filter)} `;
         break;
       }
-      case 'nestedFlowFilters': {
-        const filter = params[unmutableKey];
-        if (filter && JSON.stringify(filter) !== '{}')
-          queryParams = queryParams.concat(
-            unmutableKey,
-            `:${parseFilterToGraphQL(filter)} `
-          );
+      default: {
+        if (key !== 'signal' && params[key]) {
+          queryParams += `${key}: ${stringify(params[key])} `;
+        }
         break;
       }
     }
   }
-
-  return queryParams === '' ? '' : `(${queryParams})`;
+  return !queryParams ? '' : `(${queryParams})`;
 };
