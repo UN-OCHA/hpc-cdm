6666c23dd0869bc7510614f1ccd6b54dede47b15
Onitoxan <anpesalado@gmail.com>
2024-07-31 14:58:00 +0200

    Add InfoAlert component and minor fix to export of MergeModal

--- apps/hpc-ftsadmin/src/app/components/tables/table-utils.tsx
+++ apps/hpc-ftsadmin/src/app/components/tables/table-utils.tsx
@@ -11,8 +11,6 @@ import EllipsisText from '../../utils/ellipsis-text';
 import { Chip, IconButton, TableRow, Tooltip } from '@mui/material';
 import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
 import { C } from '@unocha/hpc-ui';
-import { util } from '@unocha/hpc-core';
-import { LocalStorageSchema } from '../../utils/local-storage-type';
 import type {
   FlowHeaderID,
   KeywordHeaderID,
@@ -210,13 +208,4 @@ export const RenderChipsRow = ({
     );
   }
   return chipList;
-};
-
-/**
- * Handle function to control the information text in the Draggable List components of tables */
-export const handleTableSettingsInfoClose = (
-  setTableInfoDisplay: React.Dispatch<React.SetStateAction<boolean | undefined>>
-) => {
-  util.setLocalStorageItem<LocalStorageSchema>('tableSettings', false);
-  setTableInfoDisplay(false);
 };
