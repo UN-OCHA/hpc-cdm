7b698b070995189111fc3d9ac86162e1c41e0e39
Onitoxan <anpesalado@gmail.com>
2024-10-09 11:21:22 +0200

    Replace xlxs package with exceljs

--- apps/hpc-ftsadmin/src/app/utils/download-excel.ts
+++ apps/hpc-ftsadmin/src/app/utils/download-excel.ts
@@ -1,15 +1,15 @@
 // TODO: Check if this should be done on backend side, Currently not working properly
-import * as XLSX from 'xlsx';
+import { Workbook } from 'exceljs';
 export const downloadExcel = <T>(
   data: T[],
   fileName: string
 ): Promise<void> => {
   return new Promise((resolve, reject) => {
     try {
-      const worksheet = XLSX.utils.json_to_sheet<T>(data);
-      const workbook = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
-      XLSX.writeFile(workbook, `${fileName}.xlsx`);
+      const workbook = new Workbook();
+      const worksheet = workbook.addWorksheet('Sheet1');
+      worksheet.addRows(data);
+      workbook.xlsx.writeFile(`${fileName}.xlsx`);
       resolve();
     } catch (error) {
       reject(error);
