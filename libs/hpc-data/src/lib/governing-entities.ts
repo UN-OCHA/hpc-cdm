099a03caf2e9062f1bb0bc0b1c18a844db4c89f2
Onitoxan <anpesalado@gmail.com>
2024-10-18 15:43:28 +0200

    Add autofill logic for `globalClusters` fields

--- libs/hpc-data/src/lib/governing-entities.ts
+++ libs/hpc-data/src/lib/governing-entities.ts
@@ -37,8 +37,34 @@ export const GET_GOVERNING_ENTITY_RESULT = t.intersection([
 export type GetGoverningEntityResult = t.TypeOf<
   typeof GET_GOVERNING_ENTITY_RESULT
 >;
+
+export const GET_GOVERNING_ENTITIES_BY_PLAN_ID_PARAMS = t.type({
+  planId: t.number,
+  excludeAttachments: t.union([t.boolean, t.undefined]),
+});
+
+export type GetGoverningEntitiesByPlanIdParams = t.TypeOf<
+  typeof GET_GOVERNING_ENTITIES_BY_PLAN_ID_PARAMS
+>;
+
+export const GET_GOVERNING_ENTITIES_BY_PLAN_ID_RESULT = t.array(
+  t.intersection([
+    GOVERNING_ENTITY,
+    t.type({
+      governingEntityVersion: GOVERNING_ENTITY_VERSION,
+      globalClusterIds: t.array(t.number),
+    }),
+  ])
+);
+
+export type GetGoverningEntitiesByPlanIdResult = t.TypeOf<
+  typeof GET_GOVERNING_ENTITIES_BY_PLAN_ID_RESULT
+>;
 export interface Model {
   getGoverningEntity(
     params: GetGoverningEntityParams
   ): Promise<GetGoverningEntityResult>;
+  getGoverningEntitiesByPlanId(
+    params: GetGoverningEntitiesByPlanIdParams
+  ): Promise<GetGoverningEntitiesByPlanIdResult>;
 }
