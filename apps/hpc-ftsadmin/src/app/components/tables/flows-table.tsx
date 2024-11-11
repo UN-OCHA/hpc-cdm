860ff74e2d88a5968f94192d041c43bc57e8d85a
Onitoxan <anpesalado@gmail.com>
2024-11-11 16:59:27 +0100

    Modify snackbar color of pending flows list

--- apps/hpc-ftsadmin/src/app/components/tables/flows-table.tsx
+++ apps/hpc-ftsadmin/src/app/components/tables/flows-table.tsx
@@ -714,13 +714,16 @@ export default function FlowsTable(props: FlowsTableProps) {
               <Portal>
                 <Snackbar
                   open={values.flows.length > 0}
+                  anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                  sx={tw`rounded-[4px] bg-unocha-primary`}
+                  ContentProps={{ sx: tw`rounded-[4px] bg-unocha-primary` }}
                   message={t.t(
                     lang,
                     (s) => s.components.flowsTable.rejectPendingFlows.message
                   )}
                   action={
                     <C.ButtonSubmit
-                      color="neutral"
+                      color="primary_light"
                       text={t.t(
                         lang,
                         (s) => s.components.flowsTable.rejectPendingFlows.button
