695fed2a0c484628a150eb2193eb3728b39457ea
Onitoxan <anpesalado@gmail.com>
2024-12-16 09:53:28 +0100

    Add custom `useQueryParams` hook
    It's built on top of `useSearchParams` from `react-router`.

---
+++ apps/hpc-ftsadmin/src/app/utils/useQueryParams.tsx
@@ -0,0 +1,69 @@
+import t from 'io-ts';
+import { type NavigateOptions, useSearchParams } from 'react-router';
+import { isRight } from 'fp-ts/lib/Either';
+
+type Props<T> = {
+  codec: t.Type<T>;
+  initialValues: T;
+};
+
+function toURLSearchParams<T>(params: T): URLSearchParams {
+  const searchParams = new URLSearchParams();
+  for (const key in params) {
+    if (params[key] !== undefined) {
+      searchParams.set(key, String(params[key]));
+    }
+  }
+  return searchParams;
+}
+
+function useQueryParams<T extends Record<string, string | number>>({
+  codec,
+  initialValues,
+}: Props<T>) {
+  const stringifiedInitialValues: Record<string, string> = {};
+  for (const key in initialValues) {
+    stringifiedInitialValues[key] = String(initialValues[key]);
+  }
+
+  const [searchParams, setSearchParams] = useSearchParams(
+    stringifiedInitialValues
+  );
+
+  const decodeParams = (params: URLSearchParams): T => {
+    const obj: Record<string, string> = {};
+    for (const [key, value] of params) {
+      if (value !== undefined) {
+        obj[key] = value;
+      }
+    }
+
+    const result = codec.decode(obj);
+
+    if (!isRight(result)) {
+      console.error('Invalid query params:', result);
+      console.warn('Reverting back to initial values...');
+      return initialValues;
+    }
+
+    return result.right;
+  };
+
+  const setValidatedSearchParams = (
+    newParams: T,
+    flushSync?: NavigateOptions['flushSync']
+  ) => {
+    const result = codec.decode(newParams);
+
+    if (!isRight(result)) {
+      console.error('Invalid parameters to set:', result.left);
+      return;
+    }
+    const validatedParams = toURLSearchParams(newParams);
+    setSearchParams(validatedParams, { flushSync });
+  };
+
+  return [decodeParams(searchParams), setValidatedSearchParams] as const;
+}
+
+export default useQueryParams;
