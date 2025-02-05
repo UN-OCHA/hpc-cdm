0255f19389fdcbcdb4a12721552ea6adb5578ab9
Onitoxan <anpesalado@gmail.com>
2025-02-05 13:00:46 +0100

    Remove space of flowID

--- apps/hpc-ftsadmin/src/app/components/tables/flows-table.tsx
+++ apps/hpc-ftsadmin/src/app/components/tables/flows-table.tsx
@@ -251,7 +251,7 @@ export default function FlowsTable(props: FlowsTableProps) {
                       data-test="flows-table-id"
                     >
                       <Link to={paths.flow(row.id, row.versionID)}>
-                        {row.id} v{row.versionID}
+                        {row.id}v{row.versionID}
                       </Link>
                     </TableCell>
                   );
