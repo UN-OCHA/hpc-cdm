d0b0186a14119ba8b45c3308358faab47e7aae53
Onitoxan <anpesalado@gmail.com>
2025-03-19 15:19:09 +0100

    Fix keyword table starting on end

--- apps/hpc-ftsadmin/src/app/components/tables/keywords-table.tsx
+++ apps/hpc-ftsadmin/src/app/components/tables/keywords-table.tsx
@@ -534,12 +534,11 @@ const KeywordTable = (props: KeywordTableProps) => {
                   </Modal>
                 </TopRowContainer>
               </ChipDiv>
-              <Box sx={{ overflowX: 'auto', transform: 'rotateX(180deg)' }}>
+              <Box sx={{ overflowX: 'auto' }}>
                 <TableContainer
                   sx={{
                     width: '100%',
                     display: 'table',
-                    transform: 'rotateX(180deg)',
                     tableLayout: 'fixed',
                     lineHeight: '1.35',
                     fontSize: '1.32rem',
